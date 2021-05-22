321e868b9d17d26b99aea13be8fdd78118d71691
shaleen-hub <80944708+shaleen-hub@users.noreply.github.com>
2021-05-22 22:22:07 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,149 +1,146 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint
-var world;
-var box1;
+const Constraint = Matter.Constraint;
 
-var connection;
-var box,box2,box3,box4,box5,box6,box7,box8,box9,box10,box11,box12,box13,box14,box15, 
-    box16,box17,box18,box19,box20,box21,box22,box23,box24,box25,box26,box27,box28,
-    box29;
+var engine, world;
+var block1, block2, block3, block4, block5, block6, block7, block8, block9;
+var block10,block11,block12, block13, block14;
+var player;
+var ground1, ground2, ground3;
 
-var stand1,stand2;
+var block15, block16, block17, block18, block19, block20, block21;
+var block22;
+var sling;
+var player;
+
 var ball;
-var score;
-var ground1;
+var img;
 
+function preload(){
+  img = loadImage("hexagon (1).png");
 
-function preload() {
-     
 }
 
-function setup(){
-    var canvas = createCanvas(1400,800);
-    engine = Engine.create();
+function setup() {
+  createCanvas(1500,600);
+  engine = Engine.create();
     world = engine.world;
 
-    stand1 = new Ground(500,500,450,10);
-    stand2 = new Ground(1000,250,450,10);
 
-    box  = new Box(430,490,50,50);
-    box2 = new Box(450,490,50,50);
-    box3 = new Box(470,490,50,50);
-    box4 = new Box(490,490,50,50);
-    box5 = new Box(510,490,50,50);
-    //
-    box6 = new Box1(445,450,40,40);
-    box7 = new Box1(465,450,40,40);
-    box8 = new Box1(485,450,40,40);
-    box9 = new Box1(505,450,40,40);
-    //
-    box10 = new Box2(460,430,30,30);
-    box11 = new Box2(480,430,30,30);
-    box12 = new Box2(500,430,30,30);
-    //
-    box13 = new Box3(465,410,20,20);
-    box14 = new Box3(485,410,20,20);
-    //
-    box15 = new Box4(475,360,20,20);
-score=0;
-    box16 = new Box5(900,240,40,40);
-    box17 = new Box5(860,240,40,40);
-    box18 = new Box5(940,240,40,40);
-    box19 = new Box5(820,240,40,40);
-    box20 = new Box5(980,240,40,40);
-    box21 = new Box6(920,200,40,40);
-    box22 = new Box6(890,200,40,40);
-    box23 = new Box6(950,200,40,40);
-    box24 = new Box6(860,200,40,40);
-    box25 = new Box7(890,160,40,40);
-    box26 = new Box7(930,160,40,40);
-    box27 = new Box7(880,160,40,40);
-    box28 = new Box8(930,120,40,40);
-    box29 = new Box8(890,120,40,40);
+  block1 = new Block(600,260,30,40);
+  block2 = new Block(570,260,30,40);
+  block3 = new Block(540,260,30,40);
+  block4 = new Block(630,260,30,40);
+  block5 = new Block(660,260,30,40);
 
-    ball = new Ball(150,400,44);
 
-    connection = new slingshot({x:150,y:400},ball.body);
-  }
-function draw(){
-    background(0);    
-    Engine.update(engine);
 
-    stand1.display();
-    stand2.display();
-    text ("Score "+score,width-300,50);
+  block6 = new Block(585,220,30,40);
+  block7 = new Block(555,220,30,40);
+  block8 = new Block(615,220,30,40);
+  block9 = new Block(645,220,30,40);
 
-   box.score();
-    box2.score();
-    box3.score();
-    box4.score();
-    box5.score();
-    box6.score();
-    box7.score();
-    box8.score();
-    box9.score();
-    box10.score();
-    box11.score();
-    box12.score();
-    box13.score();
-    box14.score();
-    box15.score(); 
-    box16.score();
-    box17.score();
-    box18.score();
-    box19.score();
-    box20.score();
-    box21.score();
- box22.score();
-    box23.score();
-    box24.score();
-    box25.score();
-    box26.score();
-      box27.score();
-  
-  
- 
-    box.display();
-    box2.display();
-    box3.display();
-    box4.display();
-    box5.display();
-    box6.display();
-    box7.display();
-    box8.display();
-    box9.display();
-    box10.display();
-    box11.display();
-    box12.display();
-    box13.display();
-    box14.display();
-    box15.display(); 
-    box16.display();
-    box17.display();
-    box18.display();
-    box19.display();
-    box20.display();
-    box21.display();
-    box22.display();
-    box23.display();
-    box24.display();
-    box25.display();
-    box26.display();
-    box28.display();
-  box27.display();
-    
-    box29.display();
-    connection.display();
-    
-    ball.display();
+
+
+  bolck10 = new Block(600,170,30,40);
+  block11 = new Block(570,180,30,40);
+  block12 = new Block(630,180,30,40);
+
+
+  block13 = new Block(600,140,30,40);
+
+  ground1 = new Ground(600,285,200,10);
+  ground2 = new Ground(900,195,200,10);
+  ground3 = new Ground(750, 600, 1500, 10)
+
+
+  block14 = new Block(900,170,30,40);
+  block15 = new Block(930,170,30,40);
+  block16 = new Block(870,170,30,40);
+  block17 = new Block(840,170,30,40);
+  block18 = new Block(960,170,30,40);
+
+  block19 = new Block(900,140,30,40);
+  block20 = new Block(930,140,30,40);
+  block21 = new Block(870,140,30,40);
+
+  block22 = new Block(900,110,30,40);
+
+  player = new Player(50,200,30,30);
+
+  ball = Bodies.circle(50,200,20);
+  World.add(world,ball);
+
+  sling = new Chain(this.ball,{x:150, y:160});
+
 }
+
+function draw() {
+  background("grey");
+  Engine.update(engine);
+
+  fill(rgb(135, 205, 236));
+
+  block1.display();
+  block2.display();
+  block3.display();
+  block4.display();
+  block5.display();
+
+  fill("lightBlue");
+  block6.display();
+  block7.display();
+  block8.display();
+  block9.display();
+
+  fill("lightPink");
+  bolck10.display();
+  block11.display();
+  block12.display();
+  fill(rgb(47, 48, 48));
+
+  block13.display();
+
+  fill(rgb(135, 205, 236));
+  block14.display();
+  block15.display();
+  block16.display();
+  block17.display();
+  block18.display();
+  fill("lightGreen");
+
+  block19.display();
+  block20.display();
+  block21.display();
+  fill("lime");
+
+  block22.display();
+
+  ground1.display();
+  ground2.display();
+  ground3.display();
+
+  imageMode(CENTER);
+  image(img,ball.position.x,ball.position.y,40,40);
+
+  sling.display();
+}
+
 
 function mouseDragged(){
-  Matter.Body.setPosition(ball.body,{x: mouseX, y: mouseY});
+  Matter.Body.setPosition(this.ball, {x: mouseX , y: mouseY});
 }
 
+
 function mouseReleased(){
-  connection.fly();
+  sling.fly();
+}
+
+function keyPressed(){
+	if(keyCode === 32)
+	{
+	
+    sling.attach(this.ball);
+	}
 }
